6b4ce3310e8f4146421988531d6e689be22db075
DHMP91 <dhmp91@gmail.com>
2026-03-15 20:36:41 -0400

    add inits

--- scanner/main.py
+++ scanner/main.py
@@ -3,7 +3,7 @@
 
 import os
 import logging
-from scanner.lib.waveshare_epd import epd7in5_V2
+from lib.waveshare_epd import epd7in5_V2
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
