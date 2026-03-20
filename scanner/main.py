5f924687fa18086e84994983f6796601f9e46c4b
DHMP91 <dhmp91@gmail.com>
2026-03-20 15:33:45 -0400

    move scanning and displaying to different threads

--- scanner/main.py
+++ scanner/main.py
@@ -3,63 +3,81 @@
 
 import logging
 import os
-import platform
-import sys
+import queue
 import time
-import traceback
+import threading
 from scanner.waveshare_epd import epd7in5_V2
 from scanner.src.scanner_reader import ScannerReader
 from PIL import Image,ImageDraw,ImageFont
 logging.basicConfig(level=logging.INFO)
 
 
-# Scanner Reader
-try:
+def start_scanner(q):
+    scanner_name = "NT CCD barcode scanner"
+    scanner_reader = ScannerReader()
+    device = scanner_reader.find_scanner(scanner_name)
     while True:
-        scanner_name = "NT CCD barcode scanner"
-        scanner_reader = ScannerReader()
-        device = scanner_reader.find_scanner(scanner_name)
-        for i in range(3):
-            if device == None:
-                logging.info(f"{scanner_name} not found. Trying again in 10s...")
-                time.sleep(10)
-                device = scanner_reader.find_scanner(scanner_name)
-            else:
-                logging.info(f"Scanner {scanner_name} found!")
-                break
-
         if device == None:
-            raise Exception(f"{scanner_name} was not found after 3 attemps")
+            logging.info(f"{scanner_name} not found. Trying again in 10s...")
+            time.sleep(5)
+            device = scanner_reader.find_scanner(scanner_name)
+        else:
+            logging.info(f"Scanner {scanner_name} found!")
+            break
 
+    if device == None:
+        raise Exception(f"{scanner_name} was not found after 3 attemps")
+    
+    while True:
         barcode = scanner_reader.read_scanner(device)
         logging.info(f"barcode scanner: {barcode}")
 
-        picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
-        font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-        
-        logging.info("Scanner Demo")
-        epd = epd7in5_V2.EPD()
-        
-        logging.info("init and Clear")
-        epd.init()
-        epd.Clear()
 
-        logging.info("init and Clear")
-        barcode_label = 'Bar Code Scanned:'
-        num_spacer = 5
-        Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-        draw = ImageDraw.Draw(Himage)
-        draw.text((10, 0), f'{barcode_label}: {barcode}', font = font24, fill = 0)
-        draw.line((20, 50, 70, 100), fill = 0)
-        epd.display(epd.getbuffer(Himage))
-        time.sleep(2)
+def display(q):
+    try:
+        while True:
+            # Loop until something is put for display
+            barcode = q.get(timeout=1) 
+            while barcode is None:
+                time.sleep(1)
+                barcode = q.get(timeout=1) 
 
-        logging.info("Goto Sleep...")
-        epd.sleep()
+            # Display the bar code
+            picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+            font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+            
+            logging.info("Scanner Demo")
+            epd = epd7in5_V2.EPD()
+            
+            logging.info("init and Clear")
+            epd.init()
+            epd.Clear()
+
+            barcode_label = 'Bar Code Scanned:'
+            num_spacer = 5
+            Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+            draw = ImageDraw.Draw(Himage)
+            draw.text((10, 0), f'{barcode_label}: {barcode}', font = font24, fill = 0)
+            draw.line((20, 50, 70, 100), fill = 0)
+            epd.display(epd.getbuffer(Himage))
+            time.sleep(10)
+
+            logging.info("Goto Sleep...")
+            epd.sleep()
+    finally:  
+        epd7in5_V2.epdconfig.module_exit(cleanup=True)
+    
+# Start listening to scan and display
+try:
+    q = queue.Queue()
+    scanner_thread = threading.Thread(target=start_scanner, args=(q,), daemon=True, name='Scanner-')
+    display_thread = threading.Thread(target=display, args=(q,), daemon=True, name='Display-')
+    scanner_thread.start()
+    display_thread.start()
+    display_thread.join()
+    scanner_thread.join()
 except IOError as e:
     logging.info(e)
-    
 except KeyboardInterrupt:    
     logging.info("ctrl + c:")
-    epd7in5_V2.epdconfig.module_exit(cleanup=True)
     exit()
