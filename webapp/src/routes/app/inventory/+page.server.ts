f0c1c950357222dd919f9ff4876f698a211b7ce9
DHMP91 <dhmp91@gmail.com>
2026-04-01 08:40:54 -0400

    form for submitting new item (front end only)

--- webapp/src/routes/app/inventory/+page.server.ts
+++ webapp/src/routes/app/inventory/+page.server.ts
@@ -1,9 +1,14 @@
 import type { Actions } from './$types';
+// import { fail } from '@sveltejs/kit';
 
 export const actions: Actions = {
 	addItem: async (event) => {
 		const formData = await event.request.formData();
 		const state = formData.get('state');
 		console.log(`Selected value on server:`, state); 
+		// return fail(422, {
+		// 	description: "test description",
+		// 	error: "test error message"
+		// });
 	}
 };
--- webapp/src/routes/app/inventory/+page.svelte
+++ webapp/src/routes/app/inventory/+page.svelte
@@ -1,32 +1,34 @@
 <script lang="ts">
+  import { slide } from "svelte/transition";
+  import type { SubmitFunction } from '@sveltejs/kit';
+  import { enhance } from '$app/forms';
   import { TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, TableSearch } from "flowbite-svelte";
   import { ButtonGroup , Button, Modal, Label, Input, Select, Helper } from "flowbite-svelte";
-  import { slide } from "svelte/transition";
-  import { type ItemType, type ItemState, type ItemCategory, type MeatSubCategory, type SeaSubCategory } from "$lib/itemType";
-  import { AllItemCategories, AllItemStates, AllMeatSubCategory, AllSeafoodSubCategory } from "$lib/itemType";
+  import { type ItemType, type ItemState, type ItemCategory, type MeatSubCategory, type SeaFoodSubCategory } from "$lib/itemType";
+  import { AllItemCategories, AllItemStates, AllMeatSubCategory, AllSeaFoodSubCategory } from "$lib/itemType";
   import { mockedItems } from "$lib/data";
- 
-  let searchTerm = $state("");
-  
-  let filteredItems = $derived.by(() => mockedItems.filter((item) => !searchTerm || item.name.toLowerCase().includes(searchTerm.toLowerCase())));
 
+  // Table
+  let searchTerm = $state("");
+  let filteredItems = $derived.by(() => mockedItems.filter((item) => !searchTerm || item.name.toLowerCase().includes(searchTerm.toLowerCase())));
   let openRow = $state();
   let details: ItemType | undefined = $state(); 
   let showDetailModal = $state(false);
-  let addItem = $state(false);
   const toggleRow = (i: number) => {
     openRow = openRow === i ? null : i;
   }
 
+
+  // Modal: New Item Form
+  let openAddItemModal = $state(false);
   let selectedCategory: ItemCategory =  $state(AllItemCategories[0]);
   let selectedMeatSubCategory: MeatSubCategory =  $state(AllMeatSubCategory[0]);
-  let selectedSeaSubCategory: SeaSubCategory =  $state(AllSeafoodSubCategory[0]);
+  let selectedSeaFoodSubCategory: SeaFoodSubCategory =  $state(AllSeaFoodSubCategory[0]);
   let selectedState: ItemState = $state(AllItemStates[0]); 
   let itemName: string = $state("")
   let initials: string = $state("")
   const existingPrefix: string[] = [...new Set(mockedItems.flatMap((item) => item.barcodePrefix ? [item.barcodePrefix] : []))]
   let prefixError = $state(false)
-
   $effect(() => {
     if(existingPrefix.includes(initials)) {
       prefixError = true
@@ -42,6 +44,26 @@
       .join('')
       .toUpperCase();
   }
+
+  let message = $state('');
+  let isLoading = $state(false);
+  const  submitNewItem: SubmitFunction = async ({ formElement }) => {
+      isLoading = true;
+      message = 'Submitting...';
+      return async ({ result, update }) => {
+        // 'result' is automatically typed based on your server action's return types
+        if (result.type === 'success') {
+          message = result.data?.message || 'Success!';
+          formElement.reset();
+        } else if (result.type === 'failure') {
+          message = result.data?.description || 'An error occurred.';
+        }
+
+        isLoading = false;
+        // 'update()' re-runs load functions to update the page state
+        await update();
+      };
+  };
 </script>
 
 <main class="flex-1 w-full overflow-auto">
@@ -77,7 +99,7 @@
     </TableBody>
   </TableSearch>
 
-  <Button class="absolute inset-e-6 bottom-20" onclick={ () => {addItem=true }}> + </Button>
+  <Button class="absolute inset-e-6 bottom-20" onclick={ () => {openAddItemModal=true }}> + </Button>
 
   <Modal title="Details" form bind:open={showDetailModal}>
     <div>
@@ -85,79 +107,76 @@
     </div>
   </Modal>
 
-  <Modal title="Add" form bind:open={addItem}>
+  <Modal title="Add" form bind:open={openAddItemModal}>
     <div>
-      <form method="POST" action="?/addItem">
-      <div class="mb-6">
-        <Label for="name" class="mb-2 block">Name</Label>
-        <Input id="name" placeholder="Enter item name" onInput={() => handleInput()} bind:value={itemName}/>
-      </div>
-
-      <div class="mb-6">
-      	{#if prefixError }
-          <Label for="barcodePrefix" color="red" class="mb-2 block">Barcode Prefix</Label>
-          <Input id="barcodePrefix" color="red" placeholder="Enter Barcode Prefix" bind:value={initials}/>
-          <Helper class="mt-2" color="red">
-            <span class="font-medium">Already exists! Manually enter a unique prefix</span>
-          </Helper>
-        {:else}
-          <Label for="barcodePrefix" class="mb-2 block">Barcode Prefix</Label>
-          <Input id="barcodePrefix" placeholder="Enter Barcode Prefix" value={initials}/>
+      <form method="POST" action="?/addItem" use:enhance={submitNewItem}>
+        {#if message}
+          <p>{message}</p>
         {/if}
-
-      </div>
-
-      <div class="mb-6">
-        <Label for="category">Select Category:</Label>
-        <Select id="category" name="category" bind:value={selectedCategory}>
-          {#each AllItemCategories as category (category)}
-            <option value={category}>{category}</option>
-          {/each}
-        </Select>
-      </div>
-
-      {#if selectedCategory === 'meat'}
         <div class="mb-6">
-          <Label for="meat">Select State:</Label>
-          <Select id="meat" name="meat" bind:value={selectedMeatSubCategory}>
-            {#each AllMeatSubCategory as meat (meat)}
-              <option value={meat}>{meat}</option>
+          <Label for="name" class="mb-2 block">Name</Label>
+          <Input id="name" placeholder="Enter item name" onInput={() => handleInput()} bind:value={itemName}/>
+        </div>
+
+        <div class="mb-6">
+          {#if prefixError }
+            <Label for="barcodePrefix" color="red" class="mb-2 block">Barcode Prefix</Label>
+            <Input id="barcodePrefix" color="red" placeholder="Enter Barcode Prefix" bind:value={initials}/>
+            <Helper class="mt-2" color="red">
+              <span class="font-medium">Already exists! Manually enter a unique prefix</span>
+            </Helper>
+          {:else}
+            <Label for="barcodePrefix" class="mb-2 block">Barcode Prefix</Label>
+            <Input id="barcodePrefix" placeholder="Enter Barcode Prefix" value={initials}/>
+          {/if}
+
+        </div>
+
+        <div class="mb-6">
+          <Label for="category">Select Item Category:</Label>
+          <Select id="category" name="category" bind:value={selectedCategory}>
+            {#each AllItemCategories as category (category)}
+              <option value={category}>{category}</option>
             {/each}
           </Select>
         </div>
-      {/if}
 
-      {#if selectedCategory === 'seafood'}
+        {#if selectedCategory === 'meat'}
+          <div class="mb-6">
+            <Label for="meat">Select Meat Type:</Label>
+            <Select id="meat" name="meat" bind:value={selectedMeatSubCategory}>
+              {#each AllMeatSubCategory as meat (meat)}
+                <option value={meat}>{meat}</option>
+              {/each}
+            </Select>
+          </div>
+        {/if}
+
+        {#if selectedCategory === 'seafood'}
+          <div class="mb-6">
+            <Label for="seafood">Select Seafood Type:</Label>
+            <Select id="seafood" name="seafood" bind:value={selectedSeaFoodSubCategory}>
+              {#each AllSeaFoodSubCategory as seafood (seafood)}
+                <option value={seafood}>{seafood}</option>
+              {/each}
+            </Select>
+          </div>
+        {/if}
+
         <div class="mb-6">
-          <Label for="seafood">Select State:</Label>
-          <Select id="seafood" name="seafood" bind:value={selectedSeaSubCategory}>
-            {#each AllSeafoodSubCategory as seafood (seafood)}
-              <option value={seafood}>{seafood}</option>
+          <Label for="state">Select State:</Label>
+          <Select id="state" name="state" bind:value={selectedState}>
+            {#each AllItemStates as state (state)}
+              <option value={state}>{state}</option>
             {/each}
           </Select>
         </div>
-      {/if}
 
-      <div class="mb-6">
-        <Label for="state">Select State:</Label>
-        <Select id="state" name="state" bind:value={selectedState}>
-          {#each AllItemStates as state (state)}
-            <option value={state}>{state}</option>
-          {/each}
-        </Select>
-      </div>
-
-      <div class="mb-6">
-        {#if prefixError }
-          <Button type="submit">
+        <div class="mb-6">
+          <Button type="submit" disabled={prefixError || isLoading}>
             Submit
           </Button>
-        {:else}
-          <Button type="submit" disabled>
-            Submit
-          </Button>
-        {/if}
-      </div>
+        </div>
       </form>
     </div>
   </Modal>
