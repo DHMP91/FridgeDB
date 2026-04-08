b46c3538a0943a6791a0d7f525be127fdb7587c9
DHMP91 <dhmp91@gmail.com>
2026-04-08 08:40:37 -0400

    require session to all routes under app and barcode

--- webapp/src/hooks.server.ts
+++ webapp/src/hooks.server.ts
@@ -1,7 +1,11 @@
 import type { Handle } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import { building } from '$app/environment';
 import { auth } from '$lib/server/auth';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
+
+
+const protectedRoutes = ['/app', "/barcode"];
 
 const handleBetterAuth: Handle = async ({ event, resolve }) => {
 	const session = await auth.api.getSession({ headers: event.request.headers });
@@ -10,6 +14,11 @@ const handleBetterAuth: Handle = async ({ event, resolve }) => {
 		event.locals.session = session.session;
 		event.locals.user = session.user;
 	}
+
+	const isProtected = protectedRoutes.some((route) => event.route.id?.startsWith(route));
+	if (isProtected && !session) {
+        throw redirect(303, '/better-auth/login');
+    }
 
 	return svelteKitHandler({ event, resolve, auth, building });
 };
