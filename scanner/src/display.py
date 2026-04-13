d3ec565eace3c5684393bfadfbeea43719527a9a
DHMP91 <dhmp91@gmail.com>
2026-04-13 19:37:22 -0400

    spacing between message and code

--- scanner/src/display.py
+++ scanner/src/display.py
@@ -30,7 +30,7 @@ class Display:
         hi_image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
         draw = ImageDraw.Draw(hi_image)
         draw.text((10, 0), f'Last Bar Code Scanned [ {code} ]', font = self.__font24, fill = 0)
-        draw.text((10, self.__new_line_spacing), f'{message}', font = self.__font24, fill = 0)
+        draw.text((20, self.__new_line_spacing), f'{message}', font = self.__font24, fill = 0)
         epd.display_Partial(
             epd.getbuffer(hi_image),
             0,
@@ -76,14 +76,6 @@ class Display:
                 fill = 0
             )
             start_line += self.__new_line_spacing
-
-        # Bottom border
-        draw.text(
-            (0, self.__y_axis_inventory_end),
-            "-"*138,
-            font = self.__font18,
-            fill = 0
-        )
 
         # Display
         epd.display_Partial(
