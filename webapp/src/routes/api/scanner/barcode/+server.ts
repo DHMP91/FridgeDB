f2dd76f96fef391217816f252a17c0d8dfbbcf35
DHMP91 <dhmp91@gmail.com>
2026-04-13 10:00:04 -0400

    clean up logic for barcode post endpoint

--- webapp/src/routes/api/scanner/barcode/+server.ts
+++ webapp/src/routes/api/scanner/barcode/+server.ts
@@ -7,30 +7,33 @@ export async function POST({ request }) {
 	const { code, force = "false" } = await request.json();
     try {
         const target: Barcode | undefined = await BarcodeModel.getBarcode(code);
+        const barcodePrefix = BarcodeModel.getPrefix(code)
+        const item = await ItemModel.getItemByBarcodePrefix(barcodePrefix)
+
+        // Check for item
+        if ( !item ){
+            return json({ error:"No item found belonging to barcode!" }, { status: 404 });
+        }
+
         if (!target) {
             // New barcode
-            const barcodePrefix = BarcodeModel.getPrefix(code)
-            const item = await ItemModel.getItemByBarcodePrefix(barcodePrefix)
-            if ( !item ){
-                return json({ error:"No item matching barcode" }, { status: 404 });
-            }
             const newBarCode: NewBarcode = {
                 code,
                 itemId: item.id
             } 
             await BarcodeModel.createBarcode(newBarCode)
-            return json({ message: `barcode created for ${item.name}!` }, { status: 201 });
+            return json({ message: `Barcode created for ${item.name}!` }, { status: 201 });
         } else if (!target.consumed) {
             // Consumed barcode
             await BarcodeModel.updateBarcode(target.id, {consumed: true})
-            return json({ message: `Setting barcode as consumed!` }, { status: 200 });
+            return json({ message: `Setting barcode as consumed for ${item.name}!` }, { status: 200 });
         } else if (target.consumed && force ) {
             // Force re-add barcode
             await BarcodeModel.updateBarcode(target.id, {consumed: false})
-            return json({ message: `Setting barcode as consumed!` }, { status: 200 });
+            return json({ message: `Resetting barcode as not consumed for ${item.name}!` }, { status: 200 });
         } else {
             // Already used barcode
-            return json({ message: `bar code already consumed!` }, { status: 410 });
+            return json({ message: `Bar code already consumed for ${item.name}!` }, { status: 410 });
         }
     } catch ( error ) {
         return json({ error }, { status: 500 });
