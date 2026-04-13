dd83e8802c0b1ca483a41ab55163bbf71490a5cf
DHMP91 <dhmp91@gmail.com>
2026-04-13 10:29:27 -0400

    move session

--- scanner/src/app_client.py
+++ scanner/src/app_client.py
@@ -9,14 +9,14 @@ class AppClient:
     def __init__(self):
         self.base_url = os.getenv("SERVER_BASE_URL")
         self.api_key = os.getenv("API_KEY")
-        self.session = aiohttp.ClientSession()
 
     async def post_barcode(self, code: str, force: bool = False) -> str:
         payload = {"code": code, "force": force }
         url = f"{self.base_url}/api/scanner/barcode"
         try:
-            async with self.session.post(url, json=payload) as resp:
-                msg = await resp.json()['message']
-                return msg
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, json=payload) as resp:
+                    msg = await resp.json()['message']
+                    return msg
         except aiohttp.ClientResponseError as e:
             return f"{e.message} ({e.status})"
