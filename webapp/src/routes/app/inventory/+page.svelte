e036e23c23861a2d70114a8894c9c8fc2acaa693
DHMP91 <dhmp91@gmail.com>
2026-04-27 06:50:42 -0400

    refactor inventory page and move table row detail into a component

--- webapp/src/routes/app/inventory/+page.svelte
+++ webapp/src/routes/app/inventory/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-  import { onDestroy } from 'svelte';
-  import { slide } from "svelte/transition";
   import type { SubmitFunction } from '@sveltejs/kit';
   import { enhance } from '$app/forms';
+  import SelectedRowDetail from './components/SelectedRowDetail.svelte';
   import { TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Table } from "flowbite-svelte"; // Table Components
-  import { Alert, Badge, ButtonGroup , Button, Modal, Label, Input, Select, Helper, Toggle} from "flowbite-svelte"; // Generic
-  import { TrashBinOutline, BarcodeOutline, PlusOutline, MinusOutline } from "flowbite-svelte-icons"; // Icons
+  import { Alert , Button, Modal, Label, Input, Select, Helper, Toggle} from "flowbite-svelte"; // Generic
+  import {BarcodeOutline } from "flowbite-svelte-icons"; // Icons
   import { type BarcodeType, type ItemType, type ItemState, type ItemCategory, type MeatSubCategory, type SeaFoodSubCategory } from "$lib/types/item";
   import { AllItemCategories, AllItemStates, AllMeatSubCategory, AllSeaFoodSubCategory } from "$lib/types/item";
   let { data } = $props();
@@ -19,24 +18,6 @@
   let openRow = $state();
   let selectedId: number | undefined = $state();
   const selectedItem = $derived(items.find((item) => item.id === selectedId));
-
-
-  //Non-barcode item QTY
-  let qtyTimeout : NodeJS.Timeout;
-  let qtyUpdateForm: HTMLFormElement | null = $state(null);
-  function updateQty () {
-    clearTimeout(qtyTimeout);
-    qtyTimeout = setTimeout(() => {
-      qtyUpdateForm?.requestSubmit(); // Triggers the native submit event
-    }, 1000);
-  }
-  const onSubmitUpdateQty: SubmitFunction = async () => {
-      return async ({ result, update }) => {
-        if (result.type === 'success') {
-          await update();
-        }
-      };
-  };
 
   // Modal: Barcodes
   let itemBarcodes: BarcodeType[]= $state([]);
@@ -124,8 +105,6 @@
       prefixError = false
     }
   });
-
-   onDestroy(() => clearTimeout(qtyTimeout));
 </script>
 
 <main class="flex-1 w-full">
@@ -167,54 +146,12 @@
               <TableBodyCell>{openRow !== i || item.barcodeControlled ? item.quantity : ""}</TableBodyCell>
             </TableBodyRow>
             {#if openRow === i}
-              <TableBodyRow class="bg-gray-50 dark:bg-gray-50 border-gray-200 border-y">
-                <TableBodyCell class="p-0">
-                  <div transition:slide={{ duration: 300, axis: "y" }}>
-                    <div class="px-4">
-                      <Badge rounded> { selectedItem!.state } </Badge>
-                      <Badge color="pink" rounded> { selectedItem!.category } </Badge>
-                      {#if selectedItem!.meat } <Badge color="red" rounded> { selectedItem!.meat } </Badge> {/if}
-                      {#if selectedItem!.seafood } <Badge color="indigo" rounded> { selectedItem!.seafood } </Badge> {/if}
-                      {#if selectedItem!.barcodeControlled }<Badge color="gray" rounded> { selectedItem!.barcodePrefix } </Badge> {/if}
-                    </div>
-                    <div class="px-4 py-4">
-                      {#if selectedItem !== null || selectedItem!== undefined }
-                        <ButtonGroup class="flex*:ring-primary-700!">
-                          <!-- <Button onclick={ async () => { 
-                            // TODO implement edit
-                          }}><EditOutline class="shrink-0 h-6 w-6" /></Button> -->
-                          {#if item.barcodeControlled }
-                            <Button onclick={ async () => { 
-                              await getBarcodes(selectedItem!.id!);
-                              showBarcodeDetailModal = true;
-                            }}><BarcodeOutline class="shrink-0 h-6 w-6" /></Button>
-                          {/if}
-                          <Button onclick={ async () => { 
-                            openDeleteModal = true;
-                          }}><TrashBinOutline class="shrink-0 h-6 w-6" /></Button>
-                        </ButtonGroup>
-                      {/if}
-                    </div>
-                  </div>
-                </TableBodyCell>
-                  <TableBodyCell class="p-0 flex items-start">
-                    {#if !selectedItem?.barcodeControlled }
-                      <form bind:this={qtyUpdateForm} method="POST" action="?/updateQty" use:enhance={onSubmitUpdateQty}>
-                        <input type="hidden" name="id" value={selectedItem!.id} />
-                        <Label for="updateQty" class="mb-1 text-sm text-gray-900 dark:text-white">Adjust quantity:</Label>
-                        <div class="relative flex items-center gap-2">
-                          <Button color="alternative" class="h-5 w-5 rounded-xl p-2" onclick={() => { selectedItem!.quantity += 1; updateQty() }}>
-                            <MinusOutline class="h-2.5 w-2.5" />
-                          </Button>
-                          <Input id="updateQty" name="updateQty" type="number" class="w-12! shrink-0 border-0 bg-transparent p-0 text-center dark:bg-transparent" placeholder="" bind:value={selectedItem!.quantity} required />
-                          <Button color="alternative" class="h-5 w-5 rounded-xl p-2" onclick={() => { selectedItem!.quantity += 1; updateQty()}}>
-                            <PlusOutline class="h-2.5 w-2.5" />
-                          </Button>
-                        </div>
-                      </form>
-                    {/if}
-                  </TableBodyCell>
-              </TableBodyRow>
+              <SelectedRowDetail 
+                {selectedItem} 
+                {getBarcodes} 
+                setShowBarcodeDetailModal = {(value: boolean) => { showBarcodeDetailModal = value} } 
+                setOpenDeleteModal = {(value: boolean) => openDeleteModal = value}
+              />
             {/if}
           {/each}
         </TableBody>
---
+++ webapp/src/routes/app/inventory/components/SelectedRowDetail.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+    import { onDestroy } from 'svelte';
+    import { slide } from "svelte/transition";
+    import type { SubmitFunction } from '@sveltejs/kit';
+    import { enhance } from '$app/forms';
+    import { TableBodyCell, TableBodyRow} from "flowbite-svelte"; // Table Components
+    import {  Badge, ButtonGroup , Button, Label, Input } from "flowbite-svelte"; // Generic
+    import { TrashBinOutline, BarcodeOutline, PlusOutline, MinusOutline } from "flowbite-svelte-icons"; // Icons
+    import { type ItemType } from "$lib/types/item";
+    
+    let { 
+        selectedItem = $bindable<ItemType>(), 
+        setShowBarcodeDetailModal,
+        getBarcodes,
+        setOpenDeleteModal,
+    } = $props<{ 
+        selectedItem: ItemType | undefined, 
+        setShowBarcodeDetailModal: (value: boolean) => void,
+        getBarcodes: (id: number) => Promise<void>,
+        setOpenDeleteModal: (value: boolean) => void,
+    }>();
+
+    //Update item quantity form (for Non-Barcode Items)
+    let itemQty = $derived(selectedItem?.quantity)
+    let qtyTimeout : NodeJS.Timeout;
+    let qtyUpdateForm: HTMLFormElement | null = $state(null);
+    function updateQty () {
+        if ( !selectedItem ){
+        return
+        }
+        clearTimeout(qtyTimeout);
+        itemQty += 1;
+        qtyTimeout = setTimeout(() => {
+        qtyUpdateForm?.requestSubmit(); // Triggers the native submit event
+        }, 1000);
+    }
+    
+    const onSubmitUpdateQty: SubmitFunction = async () => {
+        return async ({ result, update }) => {
+            if (result.type === 'success') {
+            await update();
+            }
+        };
+    };
+    onDestroy(() => clearTimeout(qtyTimeout));
+</script>
+<TableBodyRow class="bg-gray-50 dark:bg-gray-50 border-gray-200 border-y">
+    <TableBodyCell class="p-0">
+        <div transition:slide={{ duration: 300, axis: "y" }}>
+        <div class="px-4">
+            <Badge rounded> { selectedItem!.state } </Badge>
+            <Badge color="pink" rounded> { selectedItem!.category } </Badge>
+            {#if selectedItem!.meat } <Badge color="red" rounded> { selectedItem!.meat } </Badge> {/if}
+            {#if selectedItem!.seafood } <Badge color="indigo" rounded> { selectedItem!.seafood } </Badge> {/if}
+            {#if selectedItem!.barcodeControlled }<Badge color="gray" rounded> { selectedItem!.barcodePrefix } </Badge> {/if}
+        </div>
+        <div class="px-4 py-4">
+            {#if selectedItem !== null || selectedItem!== undefined }
+            <ButtonGroup class="flex*:ring-primary-700!">
+                <!-- <Button onclick={ async () => { 
+                // TODO implement edit
+                }}><EditOutline class="shrink-0 h-6 w-6" /></Button> -->
+                {#if selectedItem.barcodeControlled }
+                <Button onclick={ async () => { 
+                    await getBarcodes(selectedItem!.id!);
+                    setShowBarcodeDetailModal(true);
+                }}><BarcodeOutline class="shrink-0 h-6 w-6" /></Button>
+                {/if}
+                <Button onclick={ async () => { 
+                    setOpenDeleteModal(true);
+                }}><TrashBinOutline class="shrink-0 h-6 w-6" /></Button>
+            </ButtonGroup>
+            {/if}
+        </div>
+        </div>
+    </TableBodyCell>
+     <TableBodyCell class="p-0 flex items-start">
+        {#if !selectedItem?.barcodeControlled }
+            <form bind:this={qtyUpdateForm} method="POST" action="?/updateQty" use:enhance={onSubmitUpdateQty}>
+            <input type="hidden" name="id" value={selectedItem!.id} />
+            <Label for="updateQty" class="mb-1 text-sm text-gray-900 dark:text-white">Adjust quantity:</Label>
+            <div class="relative flex items-center gap-2">
+                <Button color="alternative" class="h-5 w-5 rounded-xl p-2" onclick={updateQty}>
+                <MinusOutline class="h-2.5 w-2.5" />
+                </Button>
+                <Input 
+                    id="updateQty" 
+                    name="updateQty" 
+                    type="number" 
+                    class="w-12! shrink-0 border-0 bg-transparent p-0 text-center dark:bg-transparent" 
+                    placeholder="" 
+                    bind:value={itemQty} 
+                    required 
+                />
+                <Button color="alternative" class="h-5 w-5 rounded-xl p-2" onclick={updateQty}>
+                <PlusOutline class="h-2.5 w-2.5" />
+                </Button>
+            </div>
+            </form>
+        {/if}
+    </TableBodyCell>
+</TableBodyRow>
