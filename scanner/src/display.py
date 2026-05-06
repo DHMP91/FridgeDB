d98fd7a5d5a05db4c07ef369dd45141c9ed73d54
DHMP91 <dhmp91@gmail.com>
2026-05-05 21:18:30 -0400

    change display laytout

--- scanner/src/display.py
+++ scanner/src/display.py
@@ -19,7 +19,7 @@ class Display:
         epd.init()
         epd.Clear()
         epd.sleep()
-        barcode_height = 70
+        barcode_height = 50
         self.__y_axis_barcode_start = self.epd.height - barcode_height
         self.__y_axis_barcode_end = self.epd.height
         self.__y_axis_inventory_start = 0
@@ -31,8 +31,8 @@ class Display:
         epd.init_part()
         hi_image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
         draw = ImageDraw.Draw(hi_image)
-        draw.text((25, 0), f'Last Barcode Scanned [ {code} ] [ {message} ]', font = self.__font20, fill = 0)
-        # draw.text((25, self.__new_line_spacing*2), f'{message}', font = self.__font20, fill = 0)
+        draw.text((0, 0), f'Last Barcode Scanned [ {code} ] [ {message} ]', font = self.__font20, fill = 0)
+        # draw.text((0, self.__new_line_spacing*2), f'{message}', font = self.__font20, fill = 0)
         epd.display_Partial(
             epd.getbuffer(hi_image),
             0,
@@ -79,12 +79,20 @@ class Display:
             )
             start_line += self.__new_line_spacing
 
+        # Bottom border
+        second_last_row = (item_per_column + 1) * self.__new_line_spacing
+        draw.text(
+            (0, second_last_row),
+             "-" * 71,
+            font = self.__font20,
+            fill = 0
+        )
         # Last updated line
-        last_row = (item_per_column + 1) * self.__new_line_spacing
+        last_row = (item_per_column + 2) * self.__new_line_spacing
         now = datetime.now()
         formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
         draw.text(
-            (25, last_row + 5),
+            (0, last_row + 5),
             'Last Updated [ ' + formatted_time + ' ]',
             font = self.__font20,
             fill = 0
